25a2433db8f077aa32f3ba6b4996f96b1dbae9c4
jawad-ul-hassan <intelligentjawad@gmail.com>
2024-10-15 04:27:37 +0500

    some changes

--- assets/js/main--s.js
+++ assets/js/main--s.js
@@ -3,7 +3,7 @@
 // variables for navbar menu toggle
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
-const navbarMenuBtn = document.querySelector(".navbar-menu-btn");
+const navbarMenuBtn = document.querySelector(".header__menu");
 const sidebarMenuBtn = document.querySelector(".sidebar-menu-btn");
 
 // variables for navbar search toggle
@@ -65,10 +65,10 @@ document.addEventListener("keydown", function (e) {
 });
 */
 const modal = document.querySelector(".modal");
-const howto = document.querySelector(".howto");
+const howto = document.querySelector(".btn--click");
 const overlay = document.querySelector(".overlay");
 const btnOpenModal = document.querySelector(".btn--download");
-const closeModalButton = document.querySelector(".close-modal");
+let closeModalButton = document.querySelector(".close-modal");
 
 const openModal = function () {
   modal.classList.remove("hidden"); // Show the modal
@@ -108,12 +108,211 @@ overlay.addEventListener("click", function (e) {
   }
 });
 
-closeModalButton.addEventListener("click", function () {
-  closeModal();
+// Open modal on button click
+btnOpenModal.addEventListener("click", function () {
+  modal.innerHTML = `<button class="close-modal">&times;</button>
+  <table class="main__table main__table--dash">
+    <thead>
+      <tr>
+        <th>DOWNLOAD LINKS</th>
+        <th>QUALITY</th>
+        <th>LANGUAGES</th>
+        <th class="tb-col-last">SIZE</th>
+      </tr>
+    </thead>
+    <div class="separator-box">
+      <div class="separator">&nbsp;</div>
+    </div>
+
+    <tbody>
+      <tr>
+        <td>
+          <div
+            class="main__table-text main__table-text--green"
+          >
+            <a
+              href="https://cdn.britannica.com/34/235834-050-C5843610/two-different-breeds-of-cats-side-by-side-outdoors-in-the-garden.jpg"
+              download
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Zm-9.71,1.71a1,1,0,0,0,.33.21.94.94,0,0,0,.76,0,1,1,0,0,0,.33-.21l4-4a1,1,0,0,0-1.42-1.42L13,12.59V3a1,1,0,0,0-2,0v9.59l-2.29-2.3a1,1,0,1,0-1.42,1.42Z"
+                />
+              </svg>
+              Download</a
+            >
+          </div>
+        </td>
+        <td>
+          <div class="main__table-text">1080p</div>
+        </td>
+        <td>
+          <div class="main__table-text">Malayalam</div>
+        </td>
+        <td>
+          <div class="main__table-text tb-col-last">
+            ....
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div
+            class="main__table-text main__table-text--green"
+          >
+            <a
+              href="https://cdn.britannica.com/34/235834-050-C5843610/two-different-breeds-of-cats-side-by-side-outdoors-in-the-garden.jpg"
+              download
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Zm-9.71,1.71a1,1,0,0,0,.33.21.94.94,0,0,0,.76,0,1,1,0,0,0,.33-.21l4-4a1,1,0,0,0-1.42-1.42L13,12.59V3a1,1,0,0,0-2,0v9.59l-2.29-2.3a1,1,0,1,0-1.42,1.42Z"
+                />
+              </svg>
+              Download</a
+            >
+          </div>
+        </td>
+        <td>
+          <div class="main__table-text">720p</div>
+        </td>
+        <td>
+          <div class="main__table-text">Telugu</div>
+        </td>
+        <td>
+          <div class="main__table-text tb-col-last">
+            ....
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div
+            class="main__table-text main__table-text--green"
+          >
+            <a
+              href="https://cdn.britannica.com/34/235834-050-C5843610/two-different-breeds-of-cats-side-by-side-outdoors-in-the-garden.jpg"
+              download
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Zm-9.71,1.71a1,1,0,0,0,.33.21.94.94,0,0,0,.76,0,1,1,0,0,0,.33-.21l4-4a1,1,0,0,0-1.42-1.42L13,12.59V3a1,1,0,0,0-2,0v9.59l-2.29-2.3a1,1,0,1,0-1.42,1.42Z"
+                />
+              </svg>
+              Download</a
+            >
+          </div>
+        </td>
+        <td>
+          <div class="main__table-text">480p</div>
+        </td>
+        <td>
+          <div class="main__table-text">Tamil</div>
+        </td>
+        <td>
+          <div class="main__table-text tb-col-last">
+            ....
+          </div>
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          <div
+            class="main__table-text main__table-text--green"
+          >
+            <a
+              href="https://cdn.britannica.com/34/235834-050-C5843610/two-different-breeds-of-cats-side-by-side-outdoors-in-the-garden.jpg"
+              download
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Zm-9.71,1.71a1,1,0,0,0,.33.21.94.94,0,0,0,.76,0,1,1,0,0,0,.33-.21l4-4a1,1,0,0,0-1.42-1.42L13,12.59V3a1,1,0,0,0-2,0v9.59l-2.29-2.3a1,1,0,1,0-1.42,1.42Z"
+                />
+              </svg>
+              Download</a
+            >
+          </div>
+        </td>
+        <td>
+          <div class="main__table-text">360p</div>
+        </td>
+        <td>
+          <div class="main__table-text">Bangla</div>
+        </td>
+        <td>
+          <div class="main__table-text tb-col-last">
+            ....
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>`;
+  openModal();
+});
+howto.addEventListener("click", function () {
+  modal.innerHTML = `<button class="close-modal">&times;</button>
+  <div class="trailer__img__box">
+    <video
+      width="100%"
+      style="border-radius: 25px;"
+      controls
+      crossorigin
+      playsinline
+      poster="./assets/iron-man-imgs/youtube-thumbnail.svg"
+    >
+      <source
+        src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+        type="video/mp4"
+        size="576"
+      />
+      <source
+        src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4"
+        type="video/mp4"
+        size="720"
+      />
+      <source
+        src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4"
+        type="video/mp4"
+        size="1080"
+      />
+
+      <track
+        kind="captions"
+        label="English"
+        srclang="en"
+        src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt"
+        default
+      />
+      <track
+        kind="captions"
+        label="Français"
+        srclang="fr"
+        src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt"
+      />
+      <a
+        href="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+        download
+        >Download</a
+      >
+    </video>
+  </div>`;
+  openModal();
 });
 
-// Open modal on button click
-btnOpenModal.addEventListener("click", openModal);
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("close-modal")) {
+    closeModal();
+  }
+});
 
 // Close modal on Escape key
 document.addEventListener("keydown", function (e) {
